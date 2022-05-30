11969c750c643f48d3f9d9cf131b3c99cca494fa
HP <HP@LAPTOP-SMC8HIAI>
2022-05-30 10:10:18 +0530

    qwerty

--- qspautomationproject/src/keyWordDrivenFramework/BaseTest.java
+++ qspautomationproject/src/keyWordDrivenFramework/BaseTest.java
@@ -13,7 +13,7 @@ public class BaseTest {
 	public void openBrowser() throws IOException
 	{
 		Flib flib = new Flib();
-		//read the data from property file (browser)123
+		//read the data from property file (browser)1233
 		String browserValue = flib.readPropertyFile("./data/config.properties","browser");
 		//read the data from property file (url)
 		String url = flib.readPropertyFile("./data/config.properties", "url");
